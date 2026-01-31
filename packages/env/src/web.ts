2d0c046f03d9db7e71cfe8e0c56cfd50ed45828a
Om Shah <shahom0306@gmail.com>
2026-01-31 10:27:56 -0800

    Security issue solved

--- packages/env/src/web.ts
+++ packages/env/src/web.ts
@@ -6,13 +6,11 @@ export const env = createEnv({
   clientPrefix: "NEXT_PUBLIC_",
   client: {
     NEXT_PUBLIC_SERVER_URL: z.string().url(),
-    NEXT_PUBLIC_SERVER_INTERNAL_API_SECRET: z.string().min(1),
     NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
   },
   runtimeEnv: {
     NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
-    NEXT_PUBLIC_SERVER_INTERNAL_API_SECRET: process.env.NEXT_PUBLIC_SERVER_INTERNAL_API_SECRET,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   },
